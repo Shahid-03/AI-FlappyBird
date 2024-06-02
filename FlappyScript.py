49f47a52f124d88a29d1fbadcc6b3ebd89cc973d
Shahid <kshahidibrahim@gmail.com>
2024-06-02 09:40:03 +0400

    python script

---
+++ FlappyScript.py
@@ -0,0 +1,37 @@
+import pygame
+import neat
+import time
+import os
+import random
+
+WIN_WIDTH = 600
+WIN_HEIGHT = 800 
+
+
+BIRD_FILENAMES = ["bird1.png", "bird2.png", "bird3.png"]
+
+BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", filename))) for filename in bird_filenames]
+
+PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", pipe.png)))
+BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", base.png)))
+BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", bg.png)))
+
+
+class Bird:
+    IMGS = BIRD_IMGS
+    MAX_ROTATION = 25 # max tilt angle when bird moves up and down
+    ROT_VEL = 20 # how much bird rotates in each frame
+    ANIMATION_TIME = 5 # duration each bird's animation in the frame
+
+    def __init__self(self,x,y): #start postition of bird
+        self.x = x
+        self.y = y
+        self.tilt = 0
+        self.tick_count = 0
+        self.velocity = 0
+        self.height = self.y
+        self.img_count = 0
+        self.img = self.IMGS[0]
+
+    def jump(self):
+        
